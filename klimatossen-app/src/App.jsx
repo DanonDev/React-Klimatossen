6c20f3c732681681299bbf2f28e6b096f4893ca0
Danylo Ruzhynskyi <100782486+foxyloverGG@users.noreply.github.com>
2023-01-11 09:26:19 +0100

    scss

--- klimatossen-app/src/App.jsx
+++ klimatossen-app/src/App.jsx
@@ -1,8 +1,15 @@
 import './App.scss';
+import Header from './Components/Header/header.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Header  
+        pageundertxt="KLIMA-TOSSEN.DK"
+        cleantxt="Clean Up" 
+        cleanudertxt="Hvad kan du gøre for at mindske din påvirkning af miljøet og naturen?"
+        />
+
     </div>
   );
 }
